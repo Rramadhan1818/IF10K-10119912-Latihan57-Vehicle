3f8c637750cf0aca607a26570c12e6f064dacc8c
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-21 19:05:25 +0700

    first add

---
+++ src/if10k/pkg10119912/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan57.vehicle;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class Bicycle extends Vehicle{
+    private int myGearCount;
+
+    public Bicycle(String myBrand, String myMOdel) {
+        super(myBrand, myMOdel);
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+}
---
+++ src/if10k/pkg10119912/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan57.vehicle;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */ 
+public class Skateboard extends Vehicle{
+    private double myBoardLenght;
+
+    public Skateboard(String myBrand, String myMOdel) {
+        super(myBrand, myMOdel);
+    }
+
+    public double getMyBoardLenght() {
+        return myBoardLenght;
+    }
+
+    public void setMyBoardLenght(double myBoardLenght) {
+        this.myBoardLenght = myBoardLenght;
+    }
+    
+}
